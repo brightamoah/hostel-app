bc27b52feb83c032e49710a1a6bac23f5c95fd96
21Coochie <brghtmalone@gmail.com>
2025-12-12 07:02:10 +0000

    Revert "Dev (#34)"
    This reverts commit 4bed1125de96ee62d637a19cc5261ccb835cd55e.

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -16,10 +16,6 @@ const EnvSchema = z.object({
   NUXT_HUB_USER_TOKEN: z.string(),
   NUXT_HUB_CLOUDFLARE_ACCOUNT_ID: z.string(),
   NUXT_HUB_CLOUDFLARE_API_TOKEN: z.string(),
-  NUXT_HUB_CLOUDFLARE_CACHE_NAMESPACE_ID: z.string(),
-  NUXT_HUB_KV_NAMESPACE_ID: z.string(),
-  NUXT_HUB_KV_NAMESPACE_PREVIEW_ID: z.string(),
-  NUXT_HUB_CLOUDFLARE_CACHE_NAMESPACE_PREVIEW_ID: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
