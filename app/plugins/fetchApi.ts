69200101f50497d5a957177e9ea159526d11a019
21Coochie <brghtmalone@gmail.com>
2026-01-06 16:28:13 +0000

    refactor(fetchApi): move csrf and headerName retrieval outside of apiFetch onRequest

--- app/plugins/fetchApi.ts
+++ app/plugins/fetchApi.ts
@@ -1,7 +1,8 @@
 export default defineNuxtPlugin(() => {
+  const { csrf, headerName } = useCsrf();
+
   const apiFetch = $fetch.create({
     onRequest({ options }) {
-      const { csrf, headerName } = useCsrf();
       options.headers = new Headers(options.headers || {});
       options.headers.append(headerName, csrf);
     },
