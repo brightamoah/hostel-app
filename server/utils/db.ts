0ce2d200718552f4e0826374cffd425669c83a7a
brightamoah <brghtmalone@gmail.com>
2026-02-27 14:05:05 +0000

    refactor: clean up database connection logic and remove unused imports in useDB function

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,7 +1,9 @@
 import type { H3Event } from "h3";
 
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/postgres-js";
+// import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
+import postgres from "postgres";
 
 import * as schema from "../db/schema/index";
 
@@ -12,17 +14,49 @@ type ContextSource = {
 };
 
 export function useDB(_eventOrContext?: ContextSource | H3Event) {
-  const runtimeConfig = useRuntimeConfig();
+  // const runtimeConfig = useRuntimeConfig();
 
-  const connectionString: string = runtimeConfig.databaseUrl;
+  // const connectionString: string = runtimeConfig.databaseUrl;
 
-  const sql = neon(connectionString!);
-  const db = drizzle(sql, {
-    schema,
-    casing: "snake_case",
+  // const sql = neon(connectionString!);
+  // const db = drizzle(sql, {
+  //   schema,
+  //   casing: "snake_case",
+  // });
+
+  // return { db };
+
+  const context = _eventOrContext?.context;
+  const config = useRuntimeConfig();
+
+  let connectionString: string | undefined;
+
+  if (import.meta.dev) {
+    connectionString = config.databaseUrl;
+  }
+  else {
+    connectionString = context?.cloudflare?.env.HYPERDRIVE.connectionString;
+  }
+
+  const client = postgres(connectionString!, {
+    prepare: false,
   });
 
-  return { db };
+  try {
+    const db = drizzle(client, {
+      schema,
+      casing: "snake_case",
+    });
+
+    return { db };
+  }
+  catch (error) {
+    console.error("Database connection failed", error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Database connection failed",
+    });
+  }
 }
 
 export type DB = ReturnType<typeof useDB>["db"];
