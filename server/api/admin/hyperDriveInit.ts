88e02510fb252278127231f5230ef09ea2738d41
brightamoah <brghtmalone@gmail.com>
2026-02-27 13:15:24 +0000

    refactor: enhance database initialization by adding schema and casing options in drizzle

--- server/api/admin/hyperDriveInit.ts
+++ server/api/admin/hyperDriveInit.ts
@@ -2,6 +2,8 @@ import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
+import * as schema from "../../db/schema/index";
+
 export default defineEventHandler(async (event) => {
   const { cloudflare } = event.context;
   const config = useRuntimeConfig();
@@ -20,17 +22,25 @@ export default defineEventHandler(async (event) => {
   });
 
   try {
-    const db = drizzle(client);
+    const db = drizzle(client, {
+      schema,
+      casing: "snake_case",
+    });
 
     const result = await db.execute(sql`SELECT NOW() AS current_time`);
 
     const listTables = await db.execute(sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`);
+
+    const users = await db.query.user.findMany({
+      where: sql`email IS NOT NULL`,
+    });
 
     return {
       success: true,
       time: result,
       hyperdrive: "connected",
       tables: listTables,
+      users,
     };
   }
   catch (e) {
